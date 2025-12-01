ea99133b7e383778eb5356eb01497d88c3dc200e
Iskander Narynbaev <isnarynbaev@ozon.ru>
2025-12-01 16:55:34 +0600

    init server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("welcome"))
+		if err != nil {
+			return
+		}
+	})
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		return
+	}
 }
